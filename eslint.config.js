73898b8341845d80104fc1bd0cb7d12ec3050c58
ACE Harish <99960959+ace4o4@users.noreply.github.com>
2026-08-13 23:34:06 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -16,8 +16,8 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
-    },
-  
+    },s
+      
     rules: {
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
